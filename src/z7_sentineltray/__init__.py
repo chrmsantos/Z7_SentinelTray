bfea5a4a437a65fddaac257a718d310305853ee2
Christian Martin dos Santos <chrmsantos@gmail.com>
2026-05-22 16:05:02 -0300

    bump: version 6.1.1

--- src/z7_sentineltray/__init__.py
+++ src/z7_sentineltray/__init__.py
@@ -1,6 +1,6 @@
 """Z7_SentinelTray package."""
 
 __author__ = "Christian Martin dos Santos"
-__version__ = "6.1.0"
-__version_label__ = "6.1.0"
+__version__ = "6.1.1"
+__version_label__ = "6.1.1"
 __release_date__ = "22-05-2026"
