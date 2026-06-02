f8e08c6543006a98491304780d2d323406055291
Christian Martin dos Santos <chrmsantos@gmail.com>
2026-06-02 13:36:32 -0300

    release: bump version to 6.2.4

--- src/z7_sentineltray/__init__.py
+++ src/z7_sentineltray/__init__.py
@@ -1,6 +1,6 @@
 """Z7_SentinelTray package."""
 
 __author__ = "CMS"
-__version__ = "6.2.3"
-__version_label__ = "6.2.3"
+__version__ = "6.2.4"
+__version_label__ = "6.2.4"
 __release_date__ = "02-06-2026"
