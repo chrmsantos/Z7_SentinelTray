d22f6493c5411bfc5f7f787751cebd753e67d8c7
Christian Martin dos Santos <chrmsantos@gmail.com>
2026-06-03 17:44:05 -0300

    release: v6.2.6 stable - remove startup update check

--- src/z7_sentineltray/__init__.py
+++ src/z7_sentineltray/__init__.py
@@ -1,6 +1,6 @@
 """Z7_SentinelTray package."""
 
 __author__ = "CMS"
-__version__ = "6.2.5"
-__version_label__ = "6.2.5"
+__version__ = "6.2.6"
+__version_label__ = "6.2.6"
 __release_date__ = "03-06-2026"
