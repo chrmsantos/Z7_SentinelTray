1c1171817cd36dfa3f244f14038f6ddfcb9087a7
Christian Martin dos Santos <chrmsantos@gmail.com>
2026-06-22 13:24:40 -0300

    Bump version to 6.2.13

--- src/z7_sentineltray/__init__.py
+++ src/z7_sentineltray/__init__.py
@@ -1,6 +1,6 @@
 """Z7_SentinelTray package."""
 
 __author__ = "CMS"
-__version__ = "6.2.12"
-__version_label__ = "6.2.12"
+__version__ = "6.2.13"
+__version_label__ = "6.2.13"
 __release_date__ = "22-06-2026"
